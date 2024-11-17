be82d3c36a6f6c7a189b034e14e662ca9c6b25cb
Wagner Rose <wagner_rose@outlook.com.br>
2024-11-17 11:38:39 -0300

    Atualizacao conexao

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,23 @@
-import { useState, useEffect } from 'react'
-
-// const url = "http://localhost:3004/categories" // conexao json server porta 3004import './App.css'
-const url = "http://localhost:3000/api/categories" // conexao rails server
-
-function App() {
-  const [categories, setCategories] = useState([])
-  const [name, setName] = useState("")
-  const [description, setDescription] = useState("")
-
-  // resgatando dados - 1
-  useEffect(() => {
-    async function getData() {
-      const res = await fetch(url); // realializa a consulta oa servidor api
-      const data = await res.json();
-      setCategories(data);
-    }
-
-    getData();
-  }, []);
-
-  // adicao de categorias - 2
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const category = {
-      name,
-      description
-    };
-
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application-json"
-      },
-      body: JSON.stringify(category)
-    });
-
-    // carregamento dinamico - 3
-
-    const addedCategory = await res.json()
-
-    setCategories((prevCategories) => [...prevCategories, addedCategory] )
-
-  };
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Appbar from './component/Appbar'
 
 
+// pages imports
+import About from './pages/About'
+import Home from './pages/Home'
 
+const App = () => {
   return (
     <div className='App'>
-      <h1>Conectar com Servidor Json</h1>
-      <ul>
-        {categories.map((category) => (
-          <li key={category.id}> {category.name} - {category.description}</li>
-        ))}
-      </ul>
-      <div className="add-category">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Categoria:
-            <input type="text" name="name" onChange={(e) => setName(e.target.value)} value={name} />
-          </label>
-          <label>
-            Descrição:
-            <input type="text" name="description" onChange={(e) => setDescription(e.target.value)} value={description} />
-          </label>
-          <button type="submit">Criar</button>
-        </form>
-      </div>
-    </div >
+      <Appbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,19 +1,72 @@
 import { useState, useEffect } from 'react'
-const url = "http://localhost:3000/categories"
 
-import "Categories.css"
+// const url = "http://localhost:3004/categories" // conexao json server porta 3004import './App.css'
+const url = "http://localhost:3000/api/categories" // conexao rails server
 
-const Categories = () => {
+function App() {
+  const [categories, setCategories] = useState([])
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
 
-    const [categories, setCategories] = useState([])
-    const [name, setName] = useState("")
-    const [description, setDescription] = useState("")
+  // resgatando dados - 1
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url); // realializa a consulta oa servidor api
+      const data = await res.json();
+      setCategories(data);
+    }
 
+    getData();
+  }, []);
 
-    
+  // adicao de categorias - 2
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const category = {
+      name,
+      description
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application-json"
+      },
+      body: JSON.stringify(category)
+    });
+
+    // carregamento dinamico - 3
+
+    const addedCategory = await res.json()
+
+    setCategories((prevCategories) => [...prevCategories, addedCategory] )
+
+  };
+
   return (
-    <div>Categories</div>
+    <div className='App'>
+      <h1>Conectar com Servidor Json</h1>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}> {category.name} - {category.description}</li>
+        ))}
+      </ul>
+      <div className="add-category">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Categoria:
+            <input type="text" name="name" onChange={(e) => setName(e.target.value)} value={name} />
+          </label>
+          <label>
+            Descrição:
+            <input type="text" name="description" onChange={(e) => setDescription(e.target.value)} value={description} />
+          </label>
+          <button type="submit">Criar</button>
+        </form>
+      </div>
+    </div >
   )
 }
 
-export default Categories
+export default App
