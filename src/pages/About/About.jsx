fa3bbc51fafbb2d3294c11cb1554fc03fffd04d6
Wagner Rose <wagner_rose@outlook.com.br>
2024-11-28 18:53:21 -0300

    Inserção de mensagem Sobre

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,18 @@ const About = () => {
     return (
         <div>
             <h1>Sobre</h1>
-        </div>
+            <p></p>
+            <h3><span>Projeto Acontece Aqui</span></h3>
+            <h3>Desenvolvido para a disciplina Hands-on-Work X</h3>
+            <h3>da Universidade do Vale do Itajaí</h3>
+            <h2>Univali</h2>
+
+            <h4>Alunos:</h4>
+            <p>Alexandre Bittencourt</p>
+            <p>Matheus Roberto da Silva</p>
+            <p>Wagner Rose</p>
+
+        </div >
     )
 }
 
